814d3b2726b85860237934510c749858c2a7159e
Bart Bloemers <bart@badjo.nl>
2022-03-23 21:56:01 +0100

    adjust contract https://docs.openzeppelin.com/contracts/4.x/wizard

--- contracts/CopernicusBeer.sol
+++ contracts/CopernicusBeer.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract CopernicusBeer is Context, Ownable, ERC721URIStorage  {
+contract CopernicusBeer is ERC721, ERC721URIStorage, Ownable  {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
@@ -16,16 +16,22 @@ contract CopernicusBeer is Context, Ownable, ERC721URIStorage  {
         //set deployer as the initial owner
     }
 
-    function createBeerNFT(string memory tokenURI) public onlyOwner() returns (uint256)  {
+    function createBeerNFT(string memory uri) public onlyOwner returns (uint256) {
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        return newTokenId;         
+        _safeMint( _msgSender() , newTokenId);
+        _setTokenURI(newTokenId, uri);
+        return newTokenId;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
     }
 
 }
