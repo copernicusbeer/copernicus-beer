154fc134cb34418e06f730c9d85815f1c6e37794
Bart Bloemers <bart@badjo.nl>
2022-03-23 15:59:56 +0100

    integrate with truffle

--- contracts/CopernicusBeer.sol
+++ contracts/CopernicusBeer.sol
@@ -1,25 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract CopernicusBeer is ERC721URIStorage, Ownable {
+contract CopernicusBeer is Context, Ownable, ERC721URIStorage  {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
 
     constructor() ERC721("Copernicus Beer", "CPB") {
-        _owner = sender();
+        //set deployer as the initial owner
     }
 
     function createBeerNFT(string memory tokenURI) public onlyOwner() returns (uint256)  {
+        _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
         return newTokenId;         
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
