2e8279ed599c283ce6d46db71b7d33ad97fae660
Bart Bloemers <bart@badjo.nl>
2022-03-23 21:58:50 +0100

    add ERC721Enumerable

--- contracts/CopernicusBeer.sol
+++ contracts/CopernicusBeer.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract CopernicusBeer is ERC721, ERC721URIStorage, Ownable  {
+contract CopernicusBeer is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable  {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIds;
@@ -26,12 +27,28 @@ contract CopernicusBeer is ERC721, ERC721URIStorage, Ownable  {
 
     // The following functions are overrides required by Solidity.
 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
+    }
+
+     function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
 }
